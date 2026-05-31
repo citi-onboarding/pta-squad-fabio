e50e1e9c8d7094ce87fba3341a4aa7b01342673c
Eduardo Feliciano <eduardofeliciano74@gmail.com>
2026-05-31 16:10:12 -0300

    refactor: refactor import logoCiti

--- mobile/src/components/ui/Header/header.tsx
+++ mobile/src/components/ui/Header/header.tsx
@@ -6,7 +6,7 @@ import { logoCiti } from "@assets";
 export default function Header() {
     return(
         <View>
-            <Text className="bg-white px-4 py-5  border-b border-gray-400 flex-row items-center text-[17px]">
+            <Text className="bg-white px-4 py-5 border-b border-gray-400 flex-row items-center text-[17px]">
                 <Image
                   source={logoCiti}
                   className="w-16 h-8 mx-3"
